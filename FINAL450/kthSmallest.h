95ce1e095d5440f98ae853dd6e6c44c6516a0176
Sagen Soren <41387786+SonicStrain@users.noreply.github.com>
2020-12-05 00:23:40 +0530

    Create kthSmallest.h

---
+++ FINAL450/kthSmallest.h
@@ -0,0 +1,19 @@
+#include<algorithm>
+//User function template for C++
+
+// arr : given array
+// l : starting index of the array i.e 0
+// r : ending index of the array i.e size-1
+// k : find kth smallest element and return using this function
+int kthSmallest(int arr[], int l, int r, int k) {
+    //code here
+    sort(arr,arr+r+1);
+    int counter = 0,value = arr[0];
+    for(int i=0;i<r+1;i++){
+        if(counter<k){
+            value = arr[i];
+            counter++;
+        }
+    }
+    return value;
+}
